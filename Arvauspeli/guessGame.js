84c9bbb7dcd557cb2556d8c03b386b1221bbab8d
MonkeyCode7 <79100835+MonkeyCode7@users.noreply.github.com>
2021-04-19 11:44:06 +0300

    Add files via upload
    Muutoksia ja päivityksiä sekä HTML, että Javascript-osuuksiin. 19.4.2021 Paul Willman

--- Arvauspeli/guessGame.js
+++ Arvauspeli/guessGame.js
@@ -1,15 +1,20 @@
-//Projekti 2: Arvauspeli/Javascript-osuus/Paul Willman/16.4.2021//
+//Projekti 2: Arvauspeli/Javascript-osuus/Paul Willman/19.4.2021//
 
 (function () {
     //Määritetään muuttujat, joita funktio tarvitsee toimiakseen.
-    var guessesLeft, randomNumber, guessInput, submitButton, answerDisplay, maxGuesses;
+    var guessesLeft, randomNumber, guessInput, guessTable = [], submitButton, answerDisplay, maxGuesses;
+    var msg1 = document.getElementById('message1');
+    var msg2 = document.getElementById('message2');
 
+    //Määritetään kuinka monta kertaa pelaaja voi veikata per peli//
     maxGuesses = 3;
+
+
 
     //Ladataan kaikki elementit DOM:mista etukäteen
     //jotta sitä ei tarvitse tehdä joka kerta, kun tarkistamme vastauksen
-    guessInput = document.getElementById("guess");
-    submitButton = document.getElementById("submitAnswer");
+guessInput = document.getElementById('guess');
+submitButton = document.getElementById("submitAnswer");
 
     //Käytetään DIV-elementtiä kertomaan vastaus
     answerDisplay = document.getElementById("answer");
@@ -27,6 +32,8 @@
 
     function initGame () {
         guessesLeft = maxGuesses;
+        msg1.textContent = "Veikkauksia jäljellä: " + guessesLeft;
+        msg2.textContent = "Arvatut numerosi ovat: None";
         randomNumber = Math.floor(Math.random() * 10 + 1);
         guessInput.value = "";
     }
@@ -34,26 +41,39 @@
 //Tarkistetaan ja vertaillaan pelaajan antamaa vastausta ehtoihin
     function checkAnswer () {
 
+      //Annetaan ohjeistukset jättämään edellinen pelaajan veikkaus näkyviin//
+      var temp = guessInput.value;
+      guessTable.push(temp);
+        //Jokainen pelaajan veikkaus vähentää yhden veikkauksen maksimi määrästä, mitä pelaaja voi tehdä per peli.
+        guessesLeft -= 1;
+
         //funktio tarkistaa ensin vastasitko oikein, jos kyllä, se pysähtyy tähän.
         if (guessInput.value == randomNumber) {
             answerDisplay.innerHTML = "Sinä voitit! " + randomNumber + " on oikein. " +
                 "Laita uusi veikkaus aloittaaksesi pelin uudelleen. Muista, että sinulla on kolme yritystä arvata lukuni!";
+            msg1.textContent = "Veikkauksia jäljellä: " + guessesLeft;
+            msg2.textContent = "Arvatut numerosi ovat: " + guessTable;
             initGame();
             return;
         }
         //Jos ei, funktio tarkistaa menitkö veikkauksessasi yli oikean vastauksen. Jos kyllä, se pysähtyy tähän.
         else if (guessInput.value > randomNumber) {
             answerDisplay.innerHTML = "Liian korkea!";
+            msg1.textContent = "Veikkauksia jäljellä: " + guessesLeft;
+            msg2.textContent = "Arvatut numerosi ovat: " + guessTable;
         }
         //Muussa tapauksessa funktio ymmärtää veikkauksen olleen liian alhainen ja se pysähtyy tähän.
         else {
             answerDisplay.innerHTML = "Liian matala!";
+            msg1.textContent = "Veikkauksia jäljellä: " + guessesLeft;
+            msg2.textContent = "Arvatut numerosi ovat: " + guessTable;
         }
-        //Jokainen pelaajan veikkaus vähentää yhden kokonaisveikkaus määrästä, mitä pelaaja voi tehdä per peli.
-        guessesLeft -= 1;
+
         //Jos kaikki 3 veikkausta meni pieleen, käynnistää funktio tämän osuuden ja lopettaa pelin, pyytäen samalla aloittamaan uuden.
         if (guessesLeft === 0) {
             answerDisplay.innerHTML += " Nyt loppui arvaukset - Sinä hävisit! Veikkaa uudelleen aloittaaksesi uuden pelin. Muista, että sinulla on kolme yritystä arvata lukuni!";
+            msg1.textContent = "Veikkauksia jäljellä: " + guessesLeft;
+            msg2.textContent = "Arvatut numerosi ovat: " + guessTable;
             initGame();
         }
     }
