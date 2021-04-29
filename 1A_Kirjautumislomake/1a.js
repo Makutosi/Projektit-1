0ce9cc5efd16f8b546ddd02b10b77e26bef80ec3
Riku Kaartoaho <rikukaartoaho@gmail.com>
2021-04-29 11:47:52 +0300

    Pieniä muutoksia funktiohin ja alerteihin

--- 1A_Kirjautumislomake/1a.js
+++ 1A_Kirjautumislomake/1a.js
@@ -1,44 +1,6 @@
 ///KIRJAUTUMISLOMAKE-TEHTÄVÄN JS/ Riku Kaartoaho/
 
-//Tarkastaa radio-painikkeet//
-function checkRadio() {
-    if(document.getElementById('user').checked || document.getElementById('student').checked || document.getElementById('none').checked) {
-
-    }else {
-        alert("Valitse millainen tietokoneen käyttäjä olet!");
-    }
-}
-//Tarkastaa checkboxit//
-function checkCheckBox() {
-    var checked = 0;
-    var checkbox = document.getElementById("checkbox");
-    var checks = checkbox.getElementsByTagName("INPUT");
-    for(var i = 0; i<checks.length; i++) {
-        if(checks[i].checked) {
-            checked++;
-        }
-    }
-
-    if (checked > 0) {
-        return true;
-    } else {
-        alert("Kerro mitä mieltä olet sivuistamme!")
-        return false;
-    }
-}
-
-function checkText() {
-    var text = document.getElementById("textarea").value;
-
-    if(text.length < 20) {
-        alert("Palautteen pitää olla vähintään 20 merkkiä pitkä!");
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function validate() {
+function validation() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var minLength = 6;
@@ -50,26 +12,53 @@ function validate() {
     } else{
         
     }
+    //Tarkastaa radio-painikkeet//
+    if(document.getElementById('user').checked || document.getElementById('student').checked || document.getElementById('none').checked) {
 
-//Tarkastaa onko salasanassa isoa kirjainta//
-    var i=0;
-    var character='';
-    while (i <= password.length){
-        character = password.charAt(i);
-        if (!isNaN(character * 1)){
-            
-        }else{
-            if (character == character.toUpperCase()) {
-                alert("Tervetuloa " + username + "!");
-                return true;
-            }
-            if (character == character.toLowerCase()){
-                alert("Salasanassa on oltava vähintään yksi iso kirjain!");
-                return false;
-            }
+    }else {
+        alert("Valitse millainen tietokoneen käyttäjä olet!");
+        return false;
+    }
+    //Tarkastaa checkboxit//
+    var checked = 0;
+    var checkbox = document.getElementById("checkbox");
+    var checks = checkbox.getElementsByTagName("INPUT");
+    for(var i = 0; i<checks.length; i++) {
+        if(checks[i].checked) {
+            checked++;
         }
-        i++;
     }
 
+    if (checked <= 0) {
+        alert("Kerro mitä mieltä olet sivuistamme!");
+        return false;
+    } else {
+        
+    }
+    //textarea check
+    var text = document.getElementById("textarea").value;
+
+    if(text.length < 20) {
+        alert("Palautteen pitää olla vähintään 20 merkkiä pitkä!");
+        return false;
+    } else {
+        return true;
+    }
 }
 
+function pwCheck() {
+    //Tarkastaa onko salasanassa isoa kirjainta//
+if(password.value != "") {
+    if(password.value.length < 6) {
+      alert("Salasan on oltava vähintään 6 merkkiä pitkä!");
+      return false;
+    }
+    var a = /[A-Z]/;
+        if(!a.test(password.value)) {
+        alert("Salasanassa on oltava vähintään yksi iso kirjain!");
+        return false;
+    }
+} else {
+    alert("Syötä salasana!");
+}
+}
